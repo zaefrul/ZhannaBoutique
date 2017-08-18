92c338ea362b2bbcbebf3b69da048fa788028030
Zaefrul Nadzrin Nasruddin <zaefrul-nadzrin.nasruddin@hpe.com>
2017-08-19 02:01:41 +0800

    Revert "zack"
    This reverts commit 7b93a7d866c006c4863a059574b0dd79bf3946c6.

--- BMSApplication/BMSApplication/Global.asax.cs
+++ BMSApplication/BMSApplication/Global.asax.cs
@@ -1,9 +1,7 @@
-﻿using BMSApplication.App_Start;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Routing;
 using System.Web.Security;
@@ -16,11 +14,8 @@ namespace BMSApplication
         void Application_Start(object sender, EventArgs e)
         {
             // Code that runs on application startup
-            AreaRegistration.RegisterAllAreas();
-            WebApiConfig.Register(GlobalConfiguration.Configuration);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
-            GlobalConfiguration.Configuration.Formatters.Remove(GlobalConfiguration.Configuration.Formatters.XmlFormatter);
+           AreaRegistration.RegisterAllAreas();
+           RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
     }
 }
